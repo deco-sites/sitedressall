58464e22177c6988e79ba63f924e10d853902349
gabriel branco <gabriel.branco@wedigi.com.br>
2024-05-13 21:32:44 -0300

    altera tipo de background categorygridcards

--- sections/Category/CategoryGrid.tsx
+++ sections/Category/CategoryGrid.tsx
@@ -87,13 +87,11 @@ function CategoryGrid(props: Props) {
   const listQTE = list?.length > 1;
   return (
     <div id={id} class="container mt-16">
-      {
-        /* <Header
+      {/* <Header
         title={header.title}
         description={header.description || ""}
         alignment={layout.headerAlignment || "center"}
-      /> */
-      }
+      /> */}
 
       <div
         class={`grid md:${
@@ -105,8 +103,8 @@ function CategoryGrid(props: Props) {
             <div
               class="absolute bg-gradient-to-b from-transparent via-transparent to-black h-full w-full rounded-3xl"
               style={{
-                backgroundImage:
-                  "linear-gradient(254.71deg, rgba(0, 0, 0, 0) 33.72%, rgba(0, 0, 0, 0.8) 100.2%)",
+                background:
+                  "linear-gradient(254deg, rgba(0, 0, 0, 0) 33%, rgba(0, 0, 0, 0.8) 100%)",
               }}
             />
             <a
