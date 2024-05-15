27e3bca5ece5563f0ca2ff8ba09d6f9ce7f35589
lucasfarias-wedigi <166856955+lucasfarias-wedigi@users.noreply.github.com>
2024-05-15 15:17:01 -0300

    fix(home): remove tab-list de componente reusableSection

--- sections/ReusableSection.tsx
+++ sections/ReusableSection.tsx
@@ -1,6 +1,3 @@
-import { h } from "preact";
-import { useSignal } from "@preact/signals";
-
 interface Tab {
   label: string;
   content: string;
@@ -8,59 +5,27 @@ interface Tab {
 
 interface Props {
   title: string;
+  descriptionTitle: string;
   description: string;
   linkText?: string;
   linkUrl?: string;
-  tabs?: Tab[];
 }
 
 export default function ReusableSection(
-  { title, description, linkText, linkUrl, tabs }: Props,
+  { title, descriptionTitle, description, linkText, linkUrl }: Props,
 ) {
-  const activeTab = useSignal(0);
-
-  function handleTabClick(index: number) {
-    activeTab.value = index;
-  }
-
   return (
     <div className="header-section">
       <h1>{title}</h1>
-      <p>{description}</p>
-      {linkText && linkUrl && (
-        <a href={linkUrl} className="link">
-          {linkText}
-        </a>
-      )}
-      {tabs && tabs.length > 0 && (
-        <div className="tab-list">
-          <div className="tab-buttons">
-            {tabs.map((tab, index) => (
-              <button
-                key={index}
-                className={`tab-button ${
-                  index === activeTab.value ? "active" : ""
-                }`}
-                data-tab-index={index}
-                onClick={() => handleTabClick(index)}
-              >
-                {tab.label}
-              </button>
-            ))}
-          </div>
-          {tabs.map((tab, index) => (
-            <div
-              key={index}
-              className={`tab-content ${
-                index === activeTab.value ? "active" : ""
-              }`}
-              data-tab-index={index}
-            >
-              {tab.content}
-            </div>
-          ))}
-        </div>
-      )}
+      <p>{descriptionTitle}</p>
+      <div>
+        <p>{description}</p>
+        {linkText && linkUrl && (
+          <a href={linkUrl} className="link">
+            {linkText}
+          </a>
+        )}
+      </div>
     </div>
   );
 }
