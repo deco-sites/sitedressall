cd5fd25b440dd095beae55fc185a9adbb0f7b564
lucasfarias-wedigi <166856955+lucasfarias-wedigi@users.noreply.github.com>
2024-05-15 16:37:14 -0300

    feat(home): Cria css do componente ReusableSection

--- sections/ReusableSection.tsx
+++ sections/ReusableSection.tsx
@@ -15,13 +15,15 @@ export default function ReusableSection(
   { title, descriptionTitle, description, linkText, linkUrl }: Props,
 ) {
   return (
-    <div className="header-section">
-      <h1>{title}</h1>
-      <p>{descriptionTitle}</p>
-      <div>
-        <p>{description}</p>
+    <div className="header-section flex justify-between container py-12 items-center">
+      <div className="max-w-[350px]">
+        <h1 className="font-berthold text-3xl font-bold pb-4">{title}</h1>
+        <p className="max-w-[654px] font-berthold text-base font-normal leading-6 text-left">{descriptionTitle}</p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <p className=" max-w-[654px] font-berthold text-base font-normal leading-6 text-left">{description}</p>
         {linkText && linkUrl && (
-          <a href={linkUrl} className="link">
+          <a href={linkUrl} className="font-berthold font-bold text-2xl leading-6 text-center w-fit rounded-3xl bg-orange-600 h-12 text-white py-3 px-8"> 
             {linkText}
           </a>
         )}
