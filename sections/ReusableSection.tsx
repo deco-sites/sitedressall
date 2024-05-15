fc28701a9935348657f9964af960037fc4bdf2dd
lucasfarias-wedigi <166856955+lucasfarias-wedigi@users.noreply.github.com>
2024-05-15 13:04:14 -0300

    Resolve bug 2

--- sections/ReusableSection.tsx
+++ sections/ReusableSection.tsx
@@ -12,41 +12,8 @@ interface Props {
 }
 
 export default function ReusableSection(
-  { title, description, linkText, linkUrl, tabs }: Props,
+  { title, description, linkText, linkUrl, tabs }: Props
 ) {
-  function handleTabClick(event: Event) {
-    const button = event.currentTarget as HTMLElement;
-    const tabIndex = button.getAttribute("data-tab-index");
-    if (tabIndex !== null) {
-      const index = parseInt(tabIndex, 10);
-      const tabButtons = document.querySelectorAll(".tab-button");
-      const tabContents = document.querySelectorAll(".tab-content");
-
-      tabButtons.forEach((btn, idx) => {
-        if (idx === index) {
-          btn.classList.add("active");
-        } else {
-          btn.classList.remove("active");
-        }
-      });
-
-      tabContents.forEach((content, idx) => {
-        if (idx === index) {
-          content.classList.add("active");
-        } else {
-          content.classList.remove("active");
-        }
-      });
-    }
-  }
-
-  // Adiciona ouvintes de eventos após o componente ser montado
-  setTimeout(() => {
-    const tabButtons = document.querySelectorAll(".tab-button");
-    tabButtons.forEach((button) => {
-      button.addEventListener("click", handleTabClick);
-    });
-  }, 0);
 
   return (
     <div className="header-section">
