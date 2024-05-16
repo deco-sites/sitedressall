e3f478394f38d4ea4b489bf0ccb0aa622937216f
gabriel branco <gabriel.branco@wedigi.com.br>
2024-05-16 13:20:21 -0300

    ajustes componente cardcarousel

--- components/cards/ImageCard.tsx
+++ components/cards/ImageCard.tsx
@@ -13,7 +13,7 @@ interface Style {
 /** @titleBy label */
 export interface Props {
   image?: ImageWidget;
-  label: string;
+  label?: string;
   description?: string;
   tag?: string;
   href?: string;
@@ -22,9 +22,8 @@ export interface Props {
 }
 
 function CardText({
-  label = "Item",
-  description = "A text describing this item",
-  tag = "Tag",
+  label,
+  description,
   alignment,
 }: {
   label?: string;
@@ -38,71 +37,48 @@ function CardText({
         !alignment || alignment === "Center" ? "items-center" : "items-start"
       }`}
     >
-      {tag && (
-        <div class="text-xs bg-primary text-primary-content py-1 px-2 rounded mb-2">
-          {tag}
-        </div>
-      )}
       {label && <h3 class="text-xl">{label}</h3>}
       {description && <div class="text-sm">{description}</div>}
     </div>
   );
 }
 
-function Card(
-  {
-    href,
-    tag,
-    label,
-    description,
-    buttonText = "Button",
-    style,
-    image =
-      "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2753/b2278d2d-2270-482b-98d4-f09d5f05ba97",
-  }: Props,
-) {
-  const position = style?.textPosition === "Bottom" ? "Bottom" : "Top";
-  const alignment = style?.textAlignment === "Left" ? "Left" : "Center";
+function Card({
+  href,
+  tag,
+  label,
+  description,
+  buttonText = "Button",
+  style,
+  image = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2753/b2278d2d-2270-482b-98d4-f09d5f05ba97",
+}: Props) {
   return (
     <div
       class="flex flex-col gap-4 justify-center"
       style={{ width: style?.width || "auto" }}
     >
       <a href={href} class="flex flex-col gap-4 lg:w-full w-full lg:h-auto">
-        {position === "Top" && (
-          <CardText
-            tag={tag}
-            label={label}
-            description={description}
-            alignment={alignment}
-          />
-        )}
         {image && (
-          <figure>
-            <Image
-              class="card"
-              src={image}
-              alt={description || label || tag}
-              width={160}
-              height={195}
-              loading="lazy"
-            />
-          </figure>
+          <div>
+            <figure>
+              <Image
+                class="card"
+                src={image}
+                alt={description || label || tag || buttonText}
+                width={160}
+                height={195}
+                loading="lazy"
+              />
+            </figure>
+            {buttonText && (
+              <a href={href} class={getButtonClasses(style?.button || {})}>
+                {buttonText}
+              </a>
+            )}
+          </div>
         )}
-        {position === "Bottom" && (
-          <CardText
-            tag={tag}
-            label={label}
-            description={description}
-            alignment={alignment}
-          />
-        )}
+        <CardText tag={tag} label={label} description={description} />
       </a>
-      {buttonText && (
-        <a href={href} class={getButtonClasses(style?.button || {})}>
-          {buttonText}
-        </a>
-      )}
     </div>
   );
 }
