e28194e8104623b1f6ace94e3bf0e78a91b58ced
Gabriel Branco <gabriel.branco@wedigi.com.br>
2024-05-21 15:48:44 +0000

    Release staging environment with 1 changes made by Lucas Farias

--- sections/TabSection.tsx
+++ sections/TabSection.tsx
@@ -1,31 +1,41 @@
 import { usePartialSection } from "deco/hooks/usePartialSection.ts";
 
-interface Props {
-  activeIndex: number;
+interface Tab {
+  label: string;
+  content: string;
 }
 
-export default function TabSection({ activeIndex }: Props) {
-  const tabsData = [
-    { label: "Aba 1", index: 0 },
-    { label: "Aba 2", index: 1 },
-    { label: "Aba 3", index: 2 },
-    { label: "Aba 4", index: 3 },
-    { label: "Aba 5", index: 4 },
-  ];
+interface Props {
+  tabs?: Tab[];
+  tabIndex?: number;
+}
+
+export default function TabSection({ tabs, tabIndex }: Props) {
+  const ti = typeof tabIndex === "number"
+    ? Math.min(Math.max(tabIndex, 0), tabs.length)
+    : 0;
 
   return (
     <div>
-      {tabsData.map((tab) => (
-        <button
-          key={tab.index}
-          className={activeIndex === tab.index ? "active" : ""}
-          {...usePartialSection<typeof TabSection>({
-            props: { activeIndex: tab.index },
-          })}
-        >
-          {tab.label}
-        </button>
-      ))}
+      <div>
+        {tabs.map((tab, index) => (
+          <button
+            class={`tab tab-lg ${index === ti ? "tab-active" : ""}`}
+            {...usePartialSection({ props: { tabIndex: index } })}
+          >
+            {tab.label}
+          </button>))
+        }
+      </div>
+      <div>
+        {tabs?.map((tab, index) => (
+          <div
+            index={index}
+          >
+            {tab.content}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
