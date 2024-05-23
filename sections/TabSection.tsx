2152248a318619476eb572391d4972f376a519aa
Lucas Farias <lucas.farias@wedigi.com.br>
2024-05-23 19:23:10 +0000

    Release prod environment with 2 changes made by Lucas Farias

--- sections/TabSection.tsx
+++ sections/TabSection.tsx
@@ -50,7 +50,7 @@ export default function TabSection({
   const infoRender = infos[indexToRender];
 
   return (
-    <div className="w-full flex container my-0 mx-auto justify-between">
+    <div className="w-full flex container my-0 mx-auto justify-between items-center">
       <div className="font-berthold text-3xl font-bold max-w-[355px]">
         {titleDiv}
       </div>
@@ -58,8 +58,9 @@ export default function TabSection({
         <ul className="flex gap-4 pb-4">
           {titles.map((title, index) => (
             <button
-              className={`tab tab-lg text-base font-medium text-center p-0 uppercase ${index === indexToRender ? "tab-active" : ""
-                }`}
+              className={`tab tab-lg text-base font-medium text-center p-0 uppercase ${
+                index === indexToRender ? "tab-active" : ""
+              }`}
               {...usePartialSection({ props: { indexActive: index } })}
             >
               {title}
