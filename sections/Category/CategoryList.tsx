82093a6fc6be637c39cb9d5642b47da5710f6f10
gabriel branco <gabriel.branco@wedigi.com.br>
2024-05-23 19:00:10 -0300

    lint

--- sections/Category/CategoryList.tsx
+++ sections/Category/CategoryList.tsx
@@ -213,7 +213,7 @@ function CategoryList(props: Props) {
                   )}
                 </div>
               </Slider.Item>
-            )
+            ),
           )}
         </Slider>
 
@@ -221,7 +221,7 @@ function CategoryList(props: Props) {
           <>
             <Slider.PrevButton
               class={clx(
-                "absolute left-0 w-11 h-11 text-blackPrimary border-blackPrimary border rounded-full flex items-center justify-center bg-white"
+                "absolute left-0 w-11 h-11 text-blackPrimary border-blackPrimary border rounded-full flex items-center justify-center bg-white",
               )}
               style={{ top: topValue }}
             >
@@ -235,7 +235,7 @@ function CategoryList(props: Props) {
 
             <Slider.NextButton
               class={clx(
-                "absolute right-0 w-11 h-11 text-blackPrimary border-blackPrimary border rounded-full flex items-center justify-center bg-white"
+                "absolute right-0 w-11 h-11 text-blackPrimary border-blackPrimary border rounded-full flex items-center justify-center bg-white",
               )}
               style={{ top: topValue }}
             >
