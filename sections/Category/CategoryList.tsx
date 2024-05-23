a2edcd297fc77f30cc127011166587513a171994
gabriel branco <gabriel.branco@wedigi.com.br>
2024-05-23 14:01:03 -0300

    lint

--- sections/Category/CategoryList.tsx
+++ sections/Category/CategoryList.tsx
@@ -51,9 +51,8 @@ function CardText({
     >
       {tag && <div class="text-sm text-blackPrimary">{tag}</div>}
       {label && <h3 class="text-lg text-blackPrimary">{label}</h3>}
-      {description && (
-        <div class="text-sm text-blackPrimary">{description}</div>
-      )}
+      {description && <div class="text-sm text-blackPrimary">{description}
+      </div>}
     </div>
   );
 }
@@ -184,7 +183,7 @@ function CategoryList(props: Props) {
                   </a>
                 )}
               </Slider.Item>
-            )
+            ),
           )}
         </Slider>
 
@@ -192,7 +191,7 @@ function CategoryList(props: Props) {
           <>
             <Slider.PrevButton
               class={clx(
-                "absolute left-0 w-11 h-11 text-blackPrimary border-blackPrimary border rounded-full flex items-center justify-center bg-white"
+                "absolute left-0 w-11 h-11 text-blackPrimary border-blackPrimary border rounded-full flex items-center justify-center bg-white",
               )}
               style={{ top: topValue }}
             >
@@ -206,7 +205,7 @@ function CategoryList(props: Props) {
 
             <Slider.NextButton
               class={clx(
-                "absolute right-0 w-11 h-11 text-blackPrimary border-blackPrimary border rounded-full flex items-center justify-center bg-white"
+                "absolute right-0 w-11 h-11 text-blackPrimary border-blackPrimary border rounded-full flex items-center justify-center bg-white",
               )}
               style={{ top: topValue }}
             >
