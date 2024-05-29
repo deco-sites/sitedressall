5df6daf517801e4b4d7a90d19c95a7d53470dd5a
lucasfarias-wedigi <166856955+lucasfarias-wedigi@users.noreply.github.com>
2024-05-29 12:26:23 -0300

    fix(home): Atualização do admin

--- sections/TabSectionSlider.tsx
+++ sections/TabSectionSlider.tsx
@@ -33,7 +33,7 @@ export default function TabSectionSlider({
     : 0;
 
   return (
-    <div className="w-full flex flex-col items-start container my-0 mx-auto justify-between max-1024:flex-col max-1024:items-start max-1024:p-4">
+    <div className="w-full flex flex-col items-start container pt-20 my-0 mx-auto justify-between max-1024:flex-col max-1024:items-start max-1024:p-4">
       <div className="font-berthold text-3xl font-bold pb-4 max-w-[390px] max-1024:text-2xl">
         {titleDiv}
       </div>
