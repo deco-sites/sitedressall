63652e60fd4d3e5060c9ea11c35911de630a2324
Cleyton Mendes <cleyton.mendest@gmail.com>
2024-06-05 14:19:17 -0300

    feat novo comp richtextv2

--- sections/Content/RichTextV2.tsx
+++ sections/Content/RichTextV2.tsx
@@ -5,34 +5,43 @@ import { HTMLWidget } from "apps/admin/widgets.ts";
  */
 
 interface Option {
-    /** @title Tipo do Texto */
-    type: 'Título' | 'Paragrafo',
-    /** @title Texto */
-    text: HTMLWidget
-    /** @title Exibe margem no topo do texto? */
-    marginTop?: boolean
+  /** @title Tipo do Texto */
+  type: "Título" | "Paragrafo";
+  /** @title Texto */
+  text: HTMLWidget;
+  /** @title Exibe margem no topo do texto? */
+  marginTop?: boolean;
 }
 
 interface Props {
-    texts: Option[]
+  texts: Option[];
 }
 
 const RichTextV2 = ({ texts }: Props) => {
-    return (
-        <div>
-            {texts.map((option) => (
-                    <>
-                        {option.type === "Título" &&
-                            <h3 class={`font-bold text-xl mb-4 lg:text-3xl${option.marginTop ? 'mt-8' : ''}`} dangerouslySetInnerHTML={{__html: option.text}}/>
-                        }
-                        {option.type === "Paragrafo" &&
-                            <div class={`mb-3 ${option.marginTop ? 'mt-3' : ''}`} dangerouslySetInnerHTML={{__html: option.text}}/>
-                        }
-                    </>
-                )
+  return (
+    <div>
+      {texts.map((option) => (
+        <>
+          {option.type === "Título" &&
+            (
+              <h3
+                class={`font-bold text-xl mb-4 lg:text-3xl${
+                  option.marginTop ? "mt-8" : ""
+                }`}
+                dangerouslySetInnerHTML={{ __html: option.text }}
+              />
             )}
-        </div>
-    )
-}
+          {option.type === "Paragrafo" &&
+            (
+              <div
+                class={`mb-3 ${option.marginTop ? "mt-3" : ""}`}
+                dangerouslySetInnerHTML={{ __html: option.text }}
+              />
+            )}
+        </>
+      ))}
+    </div>
+  );
+};
 
-export default RichTextV2
+export default RichTextV2;
