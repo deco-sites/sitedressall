b3932d84c9c10203416fc6d4b5eea58c2f67700d
Cleyton Mendes <cleyton.mendes@wedigi.com.br>
2024-06-14 15:17:50 +0000

    Release prod3 environment with 0 changes made by

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,6 +2,7 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
+import * as $$$$$$$$$0 from "./actions/postShopifyUser.ts";
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$2 from "./loaders/availableIcons.ts";
@@ -135,6 +136,9 @@ const manifest = {
     "site/sections/TabSection.tsx": $$$$$$55,
     "site/sections/TabSectionSlider.tsx": $$$$$$56,
     "site/sections/Theme/Theme.tsx": $$$$$$57,
+  },
+  "actions": {
+    "site/actions/postShopifyUser.ts": $$$$$$$$$0,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
